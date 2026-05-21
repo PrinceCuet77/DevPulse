38c766d3151fb9974a02a5d3afacbe4a8e4f404b
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2026-05-21 10:55:38 +0600

    chore: initialize Express TypeScript project with base configuration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from 'express';
+import { StatusCodes } from 'http-status-codes';
+const app: Application = express();
+
+app.get('/healthy', (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: 'The server is healthy',
+  });
+});
+
+export default app;
